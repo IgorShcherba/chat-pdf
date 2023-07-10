596873fbccb07e9f3f14030a0e4058218de0be1d
Igor Shcherba <igorolshc@gmail.com>
2023-07-10 16:47:23 +0200

    init

---
+++ .venv/htmlTemplates.py
@@ -0,0 +1,25 @@
+css = '''
+<style>
+
+.chat-message { padding: 1.5rem; border-radius:0.5rem; margin-bottom: 1rem; display: flex}
+
+.chat-message.user { background-color: #2b313e;}
+
+.chat-message.bot { background-color: #475063;}
+
+.chat-message .message { width: 85%; padding: 0 1.35rem; color: #fff; }
+</style
+
+'''
+
+bot_template='''
+<div class="chat-message bot">
+    <div className="message">{{MSG}}</div>
+</div>
+'''
+
+user_template='''
+<div class="chat-message user">
+    <div className="message">AI: {{MSG}}</div>
+</div>
+'''
